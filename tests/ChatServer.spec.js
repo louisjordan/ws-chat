548696313b9a3fd5e804d24c01ad2832c0725bf1
Louis Jordan <louisjordan@hotmail.com>
2017-03-25 09:29:32 +0000

    Refactored tests into groups

--- tests/ChatServer.spec.js
+++ tests/ChatServer.spec.js
@@ -8,73 +8,72 @@ const server = new ChatServer({ port: 8080 });
 const serverAddress = `ws://localhost:${server.port}`;
 
 // Start each test with a fresh server
-beforeEach(() => {
-  return server.open();
-});
+beforeEach(() => server.open());
 
 // Close server after each test
-afterEach(() => {
-  return server.close();
-});
+afterEach(() => server.close());
 
 describe('ChatServer', () => {
+  it('opens a WebSocket server at the port provided', () => new Promise((resolve, reject) => {
+    const client = new WebSocket(serverAddress);
 
-  it('opens a WebSocket server at the port provided', () => {
-    return new Promise(function (resolve, reject) {
+    client.addEventListener('error', () => {});
 
-      let client = new WebSocket(serverAddress);
+    client.addEventListener('unexpected-response', (req, res) => {
+      client.close();
 
-      client.addEventListener('error', () => {});
 
-      client.addEventListener('unexpected-response', (req, res) => {
-        client.close();
-
-        /* server should return a 400 Bad Request status code due to lack of nickname header - this will confirm the server is running */
-        if (res.statusCode === 400) {
-          resolve();
-        } else {
-          reject();
-        }
-      });
+      /* server should return a 400 Bad Request status code
+         due to lack of nickname header - this will confirm the server is running */
+      return res.statusCode === 400 ? resolve() : reject();
     });
-  });
+  }));
 
-  it('accepts a connection from a client that provides an unused nickname', () => {
-    return new Promise(function (resolve, reject) {
-      let options = { headers: { nickname: 'john' } };
+  describe('authentication', () => {
+    it('accepts a connection from a client that provides an unused nickname', () => new Promise((resolve, reject) => {
+      const options = { headers: { nickname: 'john' } };
 
-      let client = new WebSocket(serverAddress, options);
+      const client = new WebSocket(serverAddress, options);
 
       client.addEventListener('open', () => {
         client.close();
         resolve();
       });
 
-      client.addEventListener('error', error => {
+      client.addEventListener('error', (error) => {
         client.close();
         reject(error);
       });
-    });
-  });
+    }));
 
-  it('denies a connection from a client that provides a used nickname', () => {
-    return new Promise(function (resolve, reject) {
-      let client1options = { headers: { nickname: 'bob' } };
-      let client2options = client1options;
+    it('denies a connection from a client that provides a used nickname', () => new Promise((resolve, reject) => {
+      const client1options = { headers: { nickname: 'bob' } };
+      const client2options = client1options;
 
-      let client1 = new WebSocket(serverAddress, client1options);
+      const client1 = new WebSocket(serverAddress, client1options);
+
+
+      client1.addEventListener('error', err => reject(`Client 1 error: "${err}"`));
+
 
       // open a connection from client 1 with nickname 'john'
       client1.addEventListener('open', () => {
-        let client2 = new WebSocket(serverAddress, client2options);
+        const client2 = new WebSocket(serverAddress, client2options);
+
+
+        client2.addEventListener('error', () => { /* do nothing - let unexpected-response check the response */ });
+        client2.addEventListener('open', () => reject('Client 2 connection was accepted'));
 
 
         // expect a 401 status code as the nickname is taken
         client2.addEventListener('unexpected-response', (req, res) => {
           client1.close();
           client2.close();
-          res.statusCode === 401 ? resolve() : reject();
+          return res.statusCode === 401 ? resolve() : reject();
         });
+      });
+    }));
+  });
 
 
 
